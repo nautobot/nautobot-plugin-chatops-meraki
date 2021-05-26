a86f78f0067813a88ad996801fe2fb7aea4b0193
Jeff Kala <jeff.kala@networktocode.com>
2021-05-26 13:18:06 -0500

    update devices

--- nautobot_plugin_chatops_meraki/utils.py
+++ nautobot_plugin_chatops_meraki/utils.py
@@ -8,6 +8,11 @@ def _org_name_to_id(org_name):
     return [org['id'] for org in get_meraki_orgs() if org['name'] == org_name][0]
 
 
+def _switchname_to_id(sw_name):
+    """Translate Switch Name to Switch ID."""
+    pass
+
+
 def get_meraki_orgs():
     """Query the Meraki Dashboard API for a list of defined organizations."""
     dashboard = meraki.DashboardAPI(suppress_logging=True)
@@ -20,19 +25,19 @@ def get_meraki_org_admins(org_name):
     return dashboard.organizations.getOrganizationAdmins(_org_name_to_id(org_name))
 
 
-def meraki_devices(org_name):
+def get_meraki_devices(org_name):
     """Query the Meraki Dashboard API for a list of devices in the given organization."""
     dashboard = meraki.DashboardAPI(suppress_logging=True)
     return dashboard.organizations.getOrganizationDevices(organizationId=_org_name_to_id(org_name))
 
 
-def meraki_get_networks_by_org(org_name):
+def get_meraki_networks_by_org(org_name):
     """Query the Meraki Dashboard API for a list of Networks."""
     dashboard = meraki.DashboardAPI(suppress_logging=True)
     return dashboard.organizations.getOrganizationNetworks(_org_name_to_id(org_name))
 
 
-def meraki_get_switchports(sn):
+def get_meraki_switchports(sn):
     """Query the Meraki Dashboard API for a list of Networks."""
     dashboard = meraki.DashboardAPI(suppress_logging=True)
     return dashboard.switch.getDeviceSwitchPorts(sn)
--- nautobot_plugin_chatops_meraki/worker.py
+++ nautobot_plugin_chatops_meraki/worker.py
@@ -8,9 +8,9 @@ from nautobot_chatops.choices import CommandStatusChoices
 from .utils import (
     get_meraki_orgs,
     get_meraki_org_admins,
-    meraki_devices,
-    meraki_get_networks_by_org,
-    meraki_get_switchports,
+    get_meraki_devices,
+    get_meraki_networks_by_org,
+    get_meraki_switchports,
 )
 
 logger = logging.getLogger("rq.worker")
@@ -23,7 +23,7 @@ def cisco_meraki(subcommand, **kwargs):
 
 
 @subcommand_of("meraki")
-def get_meraki_organizations(dispatcher):
+def get_organizations(dispatcher):
     """Gather all the Meraki Organizations."""
     org_list = get_meraki_orgs()
     # If gathering information from another system may take some time, it's useful to send the user
@@ -38,7 +38,7 @@ def get_meraki_organizations(dispatcher):
 
 
 @subcommand_of("meraki")
-def get_meraki_admins(dispatcher, org_name=None):
+def get_admins(dispatcher, org_name=None):
     """Based on an Organization Name Return the Admins."""
     logger.info(f"ORG NAME: {org_name}")
     if not org_name:
@@ -46,7 +46,7 @@ def get_meraki_admins(dispatcher, org_name=None):
         org_list = get_meraki_orgs()
         # Build the list of sites, each as a pair of (user-visible string, internal value) entries
         choices = [(x["name"], x["name"]) for x in org_list]
-        dispatcher.prompt_from_menu(f"meraki get-meraki-devices", "Select Organization", choices)
+        dispatcher.prompt_from_menu(f"meraki get-admins", "Select Organization", choices)
         # Returning False indicates that the command needed to prompt the user for more information
         return False
 
@@ -65,27 +65,68 @@ def get_meraki_admins(dispatcher, org_name=None):
 
 
 @subcommand_of("meraki")
-def get_meraki_devices(dispatcher, org_name=None):
+def get_devices(dispatcher, org_name=None, device_type=None):
     """Gathers devices from Meraki API endpoint."""
     logger.info(f"ORG NAME: {org_name}")
+    logger.info(f"DEVICE TYPE: {device_type}")
     if not org_name:
-        # The user didn't specify an organization, so prompt them to pick one
         org_list = get_meraki_orgs()
-        # Build the list of sites, each as a pair of (user-visible string, internal value) entries
         choices = [(x["name"], x["name"]) for x in org_list]
-        dispatcher.prompt_from_menu(f"meraki get-meraki-devices", "Select Organization", choices)
-        # Returning False indicates that the command needed to prompt the user for more information
+        dispatcher.prompt_from_menu(f"meraki get-devices", "Select Organization", choices)
+        return False
+    if not device_type:
+        device_choices = ["All", "Access Points", "Cameras", "Firewalls", "Switches"]
+        dispatcher.prompt_from_menu(f"meraki get-devices", "Select Device Type", device_choices)
         return False
 
-    # If gathering information from another system may take some time, it's useful to send the user
     dispatcher.send_markdown(
-        f"Stand by {dispatcher.user_mention()}, I'm getting the devices at the Organization {org_name}!"
+        f"Stand by {dispatcher.user_mention()}, I'm getting {device_type} from Organization {org_name}!"
     )
-    devices = meraki_devices(org_name)
+    devices = get_meraki_devices(org_name)
     # Render the list of devices to Markdown for display to the user's chat client
     blocks = [
         dispatcher.markdown_block(f"{dispatcher.user_mention()} here are the devices at {org_name}"),
         dispatcher.markdown_block("\n".join([x["name"] for x in devices])),
+    ]
+    dispatcher.send_blocks(blocks)
+    return CommandStatusChoices.STATUS_SUCCEEDED
+
+
+def get_switches():
+    pass
+
+
+def get_firewalls():
+    pass
+
+
+def get_cameras():
+    pass
+
+
+def get_aps():
+    pass
+
+
+@subcommand_of("meraki")
+def get_networks(dispatcher, org_name=None):
+    """Gathers networks from Meraki API endpoint."""
+    logger.info(f"ORG NAME: {org_name}")
+    if not org_name:
+        org_list = get_meraki_orgs()
+        choices = [(x["name"], x["name"]) for x in org_list]
+        dispatcher.prompt_from_menu(f"meraki get-networks", "Select Organization", choices)
+        return False
+
+    dispatcher.send_markdown(
+        f"Stand by {dispatcher.user_mention()}, I'm getting the devices at the Organization {org_name}!"
+    )
+
+    networks = get_meraki_networks_by_org(org_name)
+    # Render the list of devices to Markdown for display to the user's chat client
+    blocks = [
+        dispatcher.markdown_block(f"{dispatcher.user_mention()} here are the devices at {org_name}"),
+        dispatcher.markdown_block("\n".join([x["name"] for x in networks])),
     ]
     dispatcher.send_blocks(blocks)
     return CommandStatusChoices.STATUS_SUCCEEDED
