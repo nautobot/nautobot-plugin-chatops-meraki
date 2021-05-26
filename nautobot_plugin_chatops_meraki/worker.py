7350150a846cd2a137bb1564e2fcb833dc529342
Jeff Kala <jeff.kala@networktocode.com>
2021-05-26 10:22:43 -0500

    get orgs and get org admins

--- nautobot_plugin_chatops_meraki/worker.py
+++ nautobot_plugin_chatops_meraki/worker.py
@@ -33,12 +33,45 @@ def get_meraki_organizations(dispatcher):
     # Render the list of devices to Markdown for display to the user's chat client
     blocks = [
         dispatcher.markdown_block(f"{dispatcher.user_mention()} here are the Meraki organizations"),
-        dispatcher.markdown_block("\n".join([(x["name"], x["name"]) for x in org_list])),
+        dispatcher.markdown_block("\n".join([x["name"] for x in org_list])),
     ]
 
     dispatcher.send_blocks(blocks)
 
     return CommandStatusChoices.STATUS_SUCCEEDED
+
+
+@subcommand_of("meraki")
+def get_meraki_org_admins(dispatcher, org_name=None):
+    logger.info(f"ORG NAME: {org_name}")
+    if not org_name:
+        # The user didn't specify an organization, so prompt them to pick one
+        org_list = get_meraki_orgs()
+
+        # Build the list of sites, each as a pair of (user-visible string, internal value) entries
+        choices = [(x["name"], x["name"]) for x in org_list]
+        dispatcher.prompt_from_menu(f"meraki get-meraki-devices", "Select Organization", choices)
+
+        # Returning False indicates that the command needed to prompt the user for more information
+        return False
+
+    # If gathering information from another system may take some time, it's useful to send the user
+    dispatcher.send_markdown(
+        f"Stand by {dispatcher.user_mention()}, I'm getting the admins for the Organization {org_name}!"
+    )
+
+    admins = get_meraki_org_admins(org_name)
+
+    # Render the list of devices to Markdown for display to the user's chat client
+    blocks = [
+        dispatcher.markdown_block(f"{dispatcher.user_mention()} here are the admins for {org_name}"),
+        dispatcher.markdown_block("\n".join([x["name"] for x in admins])),
+    ]
+
+    dispatcher.send_blocks(blocks)
+
+    return CommandStatusChoices.STATUS_SUCCEEDED
+
 
 @subcommand_of("meraki")
 def get_meraki_devices(dispatcher, org_name=None):
