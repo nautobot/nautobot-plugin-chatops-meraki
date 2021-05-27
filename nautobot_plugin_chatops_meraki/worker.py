02a5023ebf5c56e37c13dce297fca0c8dfda12de
Jeff Kala <jeff.kala@networktocode.com>
2021-05-27 13:08:24 -0500

    debug recent camera anaylytics

--- nautobot_plugin_chatops_meraki/worker.py
+++ nautobot_plugin_chatops_meraki/worker.py
@@ -22,6 +22,7 @@ logger = logging.getLogger("rq.worker")
 @job("default")
 def cisco_meraki(subcommand, **kwargs):
     """Interact with Meraki."""
+    logger.info(**kwargs)
     return handle_subcommands("meraki", subcommand, **kwargs)
 
 
