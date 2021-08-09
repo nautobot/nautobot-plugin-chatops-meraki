db847c2653d82c0ebe2ace0cc80e172a2085fb7f
Jeff Kala <jeff.kala@networktocode.com>
2021-08-09 15:45:04 -0500

    add test for helper functions

--- nautobot_plugin_chatops_meraki/tests/test_utils.py
+++ nautobot_plugin_chatops_meraki/tests/test_utils.py
@@ -1,1 +1,73 @@
-"""Test of utils.py"""
+"""Test of utils.py."""
+import unittest
+from unittest.mock import patch
+
+from ..utils import _org_name_to_id, _name_to_serial, _netname_to_id
+
+
+class TestUtils(unittest.TestCase):
+    """Test Version is the same."""
+
+    @patch("nautobot_plugin_chatops_meraki.utils.get_meraki_orgs")
+    def test_org_name_to_id(self, mock_orgs):
+        """Test Translate Org Name to Org Id."""
+        mock_orgs.return_value = [
+            {
+                "id": "123456",
+                "name": "NTC-TEST",
+                "url": "https://meraki.com/organization/overview",
+                "api": {"enabled": True},
+            }
+        ]
+        assert _org_name_to_id("NTC-TEST") == "123456"
+
+    @patch("nautobot_plugin_chatops_meraki.utils.get_meraki_devices")
+    def test_name_to_serial(self, mock_devices):
+        """Test Translate Name to Serial."""
+        mock_devices.return_value = [
+            {
+                "name": "sw01-test",
+                "serial": "SN987654",
+                "mac": "0c:8d:db:7e:d4:48",
+                "networkId": "L_12345",
+                "model": "MS220-8P",
+            },
+            {
+                "name": "fw01-test",
+                "serial": "SN123456",
+                "mac": "0c:8d:db:1b:5e:80",
+                "networkId": "L_12345",
+                "model": "MX64",
+            },
+        ]
+        assert _name_to_serial("NTC-TEST", "fw01-test") == "SN123456"
+
+    @patch("nautobot_plugin_chatops_meraki.utils.get_meraki_networks_by_org")
+    def test_netname_to_id(self, mock_net_name):
+        """Translate Network Name to Network ID."""
+        mock_net_name.return_value = [
+            {
+                "configTemplateId": "L_604608249974501883",
+                "enrollmentString": None,
+                "id": "L_987654321",
+                "name": "test-network-name",
+                "notes": "",
+                "organizationId": "123456",
+                "productTypes": ["appliance", "switch", "wireless"],
+                "tags": [],
+                "timeZone": "America/Chicago",
+                "url": "https://meraki.fake.com",
+            },
+            {
+                "enrollmentString": None,
+                "id": "L_123456789",
+                "name": "test-network-name-noused",
+                "notes": None,
+                "organizationId": "123456",
+                "productTypes": ["appliance", "switch", "wireless"],
+                "tags": [],
+                "timeZone": "America/New_York",
+                "url": "https://meraki.fake.com",
+            },
+        ]
+        assert _netname_to_id("NTC-TEST", "test-network-name") == "L_987654321"
