893fbc12fe3a7808b02a1f79538de52e0176aef0
Jeremy White <jeremy.white@networktocode.com>
2022-07-14 13:27:43 -0500

    preping for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,12 @@
+## v1.1.0 - 2021-09-28
+
+### Changed
+
+- Removing pinned dependency on `typing-extensions` package as not directly used.
+
+### Removed
+
+- Python 3.6 support
 ## v1.0.0 - 2021-09-28
 
 Initial release
--- nautobot_plugin_chatops_meraki/__init__.py
+++ nautobot_plugin_chatops_meraki/__init__.py
@@ -1,7 +1,13 @@
 """Plugin declaration for nautobot_plugin_chatops_meraki."""
-from nautobot.extras.plugins import PluginConfig
+try:
+    from importlib import metadata
+except ImportError:
+    # Python version < 3.8
+    import importlib_metadata as metadata
 
-__version__ = "1.1.0"
+__version__ = metadata.version(__name__)
+
+from nautobot.extras.plugins import PluginConfig
 
 
 class NautobotPluginChatopsMerakiConfig(PluginConfig):
